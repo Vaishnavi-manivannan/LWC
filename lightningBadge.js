b590f4445a586871363c30b88d1b9cf1f5ef4143
Vaishnavi-manivannan <Vaishnavi-manivannan@users.noreply.github.com>
2022-10-19 13:06:55 +0530

    Create lightningBadge.js

---
+++ lightningBadge.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class Learn1 extends LightningElement {
+    name = 'Vaishnavi';
+    company ='Capgemini';
+    designation = 'Consultant';
+    salary = '$900000';
+
+}
